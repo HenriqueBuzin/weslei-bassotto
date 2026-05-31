be7d171507a26d890288b159e392e9501e0e587c
Henrique Buzin <cultura.henrique@gmail.com>
2026-05-31 12:51:17 -0300

    G

--- frontend/src/pages/user/Home.jsx
+++ frontend/src/pages/user/Home.jsx
@@ -90,11 +90,10 @@ const testimonials = [
 ];
 
 const gallery = [
+  "/consultoria/weslei-barra-relogio-v3.jpeg",
   "/consultoria/weslei-espelho-wide-v3.jpeg",
   "/consultoria/weslei-terra-preparo-v3.jpeg",
-  "/consultoria/weslei-barra-relogio-v3.jpeg",
   "/consultoria/weslei-terra-execucao-v3.jpeg",
-  "/consultoria/weslei-barra-frontal-v3.jpeg",
 ];
 
 export default function Home() {
